c347e0492fc0f7cabfd2c19c9c1c2743323c58f1
anujeditbyanuj-cpu <anujeditbyanuj@gmail.com>
2026-05-18 13:57:40 +0530

    Update __main__.py
    Signed-off-by: anujeditbyanuj-cpu <anujeditbyanuj@gmail.com>

--- anony/__main__.py
+++ anony/__main__.py
@@ -18,7 +18,7 @@ def home():
     return "Bot is running!"
 
 def run():
-    flask_app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    flask_app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 
 def keep_alive():
     t = Thread(target=run)
